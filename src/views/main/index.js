1734e4d7def1269ff567ba24041348c89b775d67
Grzegorz Dokowicz <grzegorz.dokowicz@gmail.com>
2020-05-25 17:06:32 +0200

    Added working flexbox and BEM to views\main\index.js

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -7,16 +7,21 @@ import Text from "../../components/page-elements/text"
 
 const Main = () => {
   return (
-    <ResponsivePageContainer className={"main-container"}>
-      <div className="hero-image">
-        <Image fileName={"HeroImage.png"}/>
+    <ResponsivePageContainer className={"landing-view"}>
+      <div className="landing-view__container landing-view__container--right">
+        <div className="hero-image">
+          <Image fileName={"HeroImage.png"}/>
+        </div>
       </div>
-      <div className="text-area">
-        <Text type={"paragraph"}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi cum ea est ex facilis nulla pariatur
-          quam quas quasi, quibusdam repudiandae sit, tenetur vitae voluptatem voluptatibus voluptatum. Deserunt dolores
-          esse est expedita incidunt iusto libero nam optio quidem. Optio.
-        </Text>
+      <div className="landing-view__container landing-view__container--left">
+        <div className="text-area">
+          <Text type={"paragraph"}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi cum ea est ex facilis nulla pariatur
+            quam quas quasi, quibusdam repudiandae sit, tenetur vitae voluptatem voluptatibus voluptatum. Deserunt
+            dolores
+            esse est expedita incidunt iusto libero nam optio quidem. Optio.
+          </Text>
+        </div>
       </div>
     </ResponsivePageContainer>
   )
