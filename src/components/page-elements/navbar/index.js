4b9460c4abad7f3d8d23cb3ffafeb5a52bbd8229
Grzegorz Dokowicz <grzegorz.dokowicz@gmail.com>
2020-07-14 21:41:18 +0200

    Add hamburger-button and hamburger-button styles

--- src/components/page-elements/navbar/index.js
+++ src/components/page-elements/navbar/index.js
@@ -1,36 +1,56 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { AnchorLink } from 'gatsby-plugin-anchor-links';
 import uuid from 'react-uuid';
 
-import ResponsiveFullPageContainer from '../../containers/responsive-full-page-container';
+import ResponsivePageContainer from '../../containers/responsive-page-container';
 
 import './style.scss';
 
 const Navbar = () => {
   const routesArray = [{
     path: 'about',
-    title: 'About Me !',
+    title: 'About Me',
   }, {
     path: 'technology',
     title: 'Technology',
   },
   ];
 
+  const hamburgerRef = useRef(null);
+
+  const handleHamburgerClick = () => {
+    const activeClassName = 'hamburger__bars--active';
+    if (hamburgerRef && hamburgerRef.current.classList.contains(activeClassName)) {
+      hamburgerRef.current.classList.remove(activeClassName);
+    } else if (hamburgerRef && !hamburgerRef.current.classList.contains(activeClassName)) {
+      hamburgerRef.current.classList.add(activeClassName);
+    }
+  };
+
   return (
-    <ResponsiveFullPageContainer className="navbar">
-      <nav>
-        {routesArray.map((route) => (
-          <AnchorLink
-            key={uuid()}
-            to={`/#${route.path}`}
-            stripHash
-            className="navbar__link"
-          >
-            {route.title}
-          </AnchorLink>
-        ))}
+    <ResponsivePageContainer className="navbar__container">
+      <nav className="navbar">
+
+        <button onClick={handleHamburgerClick} className="navbar__hamburger">
+          <span ref={hamburgerRef} className="hamburger__bars" />
+        </button>
+
+        <ul className="navbar__link-list navbar__link-list-desktop">
+          {routesArray.map((route) => (
+            <li key={uuid()}>
+              <AnchorLink
+                to={`/#${route.path}`}
+                stripHash
+                className="navbar__link"
+              >
+                {route.title}
+              </AnchorLink>
+            </li>
+          ))}
+        </ul>
+
       </nav>
-    </ResponsiveFullPageContainer>
+    </ResponsivePageContainer>
   );
 };
 
