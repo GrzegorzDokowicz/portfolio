e9a1086f5a5c82f2ca62f8616cb8127cd2d3bc7c
Grzegorz Dokowicz <grzegorz.dokowicz@gmail.com>
2020-07-27 20:45:29 +0200

    Solved problem with getting elements by Id - GSAP

--- src/views/index.js
+++ src/views/index.js
@@ -3,15 +3,15 @@ import React from 'react';
 import LandingPage from './landing-page';
 import TechnologyPage from './technology';
 import Navbar from '../components/page-elements/navbar';
-import AboutMe from "./about-me";
+import AboutMe from './about-me';
 
 const App = () => (
-    <>
-        <Navbar/>
-        <LandingPage/>
-        <AboutMe/>
-        <TechnologyPage/>
-    </>
+  <>
+    <Navbar />
+    <LandingPage />
+    <AboutMe />
+    <TechnologyPage />
+  </>
 );
 
 export default App;
--- src/views/landing-page/index.js
+++ src/views/landing-page/index.js
@@ -1,33 +1,51 @@
-import React, {useRef} from 'react';
+import React from 'react';
 
 import './style.scss';
 import ResponsivePageContainer from '../../components/containers/responsive-page-container';
-import Image from "../../components/page-atoms/image";
-import Text from "../../components/page-atoms/text";
+import Image from '../../components/page-atoms/image';
+import Text from '../../components/page-atoms/text';
 
 const LandingPage = () => {
-    const animationsItems = ['plant', 'character', 'clouds', 'background_cloud']
-    const animationTarget = useRef(null)
+  const getHeroImageElements = () => {
+    const heroIds = [
+      'plant', 'character', 'clouds', 'background_cloud',
+    ];
 
+    try {
+      return heroIds.map((id) => {
+        const element = document.getElementById(id); // eslint-disable-line no-undef
 
-    return <ResponsivePageContainer id="landing_page" className="landing-view">
-        <div className="landing-view__container landing-view__container--right">
-            <div className="referance-wrapper" ref={animationTarget}>
-            <Image fileName={'LandingImage'}/>
-            </div>
-        </div>
-        <div className="landing-view__container landing-view__container--left">
-            <Text type={'title'}>
-                Grzegorz Dokowicz
-            </Text>
-            <Text type={'title-small'}>
-                Front End Developer
-            </Text>
-            <Text type={'paragraph'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt doloremque
-                doloribus laborum magnam necessitatibus, non officia quos! Animi atque beatae, cumque, dicta eaque est
-                natus quaerat quia rem sapiente vero?</Text>
-        </div>
+        if (element) {
+          return element;
+        }
+
+        throw new Error();
+      });
+    } catch ($error) {
+      return false;
+    }
+  };
+
+  return (
+    <ResponsivePageContainer id="landing_page" className="landing-view">
+      <div className="landing-view__container landing-view__container--right">
+        <Image fileName="LandingImage" afterInjection={() => getHeroImageElements()} />
+      </div>
+      <div className="landing-view__container landing-view__container--left">
+        <Text type="title">
+          Grzegorz Dokowicz
+        </Text>
+        <Text type="title-small">
+          Front End Developer
+        </Text>
+        <Text type="paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt doloremque
+          doloribus laborum magnam necessitatibus, non officia quos! Animi atque beatae, cumque,
+          dicta eaque est natus quaerat quia rem sapiente vero?
+        </Text>
+      </div>
     </ResponsivePageContainer>
+  );
 };
 
 export default LandingPage;
